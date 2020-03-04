852957a3365ffcbd24b292b2d767944280e9dd77
Ryan McDonagh <laslomcd@gmail.com>
2020-03-04 17:01:21 -0500

    started header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <h1>Hello</h1>
+    <app-header />
+    <div>
+      Content
+    </div>
+    <app-footer />
   </div>
 </template>
 
 <script>
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+
 export default {
+
+  components: {
+    'app-header': Header,
+    'app-footer': Footer
+  }
   
 }
 </script>
