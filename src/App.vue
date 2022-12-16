3d1d385c3d44a7234dfc2ffa057c5a3346ce165f
ryan <laslomcd@gmail.com>
2022-12-16 15:22:58 -0500

    rebuild

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,36 @@
 <template>
-  <div>
-    <app-header />
-    <div>
-     <app-feature></app-feature>
-     <app-menu></app-menu>
-     <app-main></app-main>
-     <app-newsletter></app-newsletter>
-    </div>
-    <app-footer />
-  </div>
+	<div>
+		<app-header />
+		<div>
+			<app-feature></app-feature>
+			<app-menu></app-menu>
+			<app-main></app-main>
+			<app-newsletter></app-newsletter>
+		</div>
+		<app-footer />
+	</div>
 </template>
 
 <script>
-
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Feature from './components/Featured/Feature';
-import Menu from './components/Menu/Menu';
-import Main from './components/CallUs/Main';
-import Newsletter from './components/Newsletter/Newsletter';
+import HeaderComponent from "./components/Header/HeaderComponent.vue";
+import FooterComponent from "./components/Footer/FooterComponent.vue";
+import FeatureComponent from "./components/Featured/FeatureComponent.vue";
+import MenuComponent from "./components/Menu/MenuComponent.vue";
+import MainComponent from "./components/CallUs/MainComponent.vue";
+import NewsletterComponent from "./components/Newsletter/NewsletterComponent.vue";
 
 export default {
-
-  components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-feature': Feature,
-    'app-menu': Menu,
-    'app-main': Main,
-    'app-newsletter': Newsletter
-  }
-  
-}
+	components: {
+		"app-header": HeaderComponent,
+		"app-footer": FooterComponent,
+		"app-feature": FeatureComponent,
+		"app-menu": MenuComponent,
+		"app-main": MainComponent,
+		"app-newsletter": NewsletterComponent,
+	},
+};
 </script>
 
 <style>
-  @import './assets/style.css';
+@import "./assets/style.css";
 </style>
---
+++ src/components/CallUs/MainComponent.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="call_us_container">
+		<div class="container">
+			<div class="first" v-scroll-reveal.reset="{ delay: 200, scale: 2 }">
+				<h1>Hungry ?</h1>
+			</div>
+			<div class="second">
+				<app-animation></app-animation>
+			</div>
+			<div class="third" v-scroll-reveal.reset="{ delay: 600, scale: 2 }">
+				<span>Give Us A Call</span> <span>419.478.1111</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import MyAnimation from "./MyAnimation";
+
+export default {
+	data() {
+		return {};
+	},
+	components: {
+		"app-animation": MyAnimation,
+	},
+};
+</script>
