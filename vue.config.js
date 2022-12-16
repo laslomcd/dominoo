9fa237acaeea12b9a2a7862b5aeed9520b31cb12
ryan <laslomcd@gmail.com>
2022-12-16 16:34:31 -0500

    config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/dominoo" : "/",
 };
